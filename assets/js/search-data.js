25750eba9c2a14b152fdcb6b14b5cdfe44c0b867
yichentong16 <yichen.tong16@gmail.com>
2025-12-14 19:07:13 +0000

    Deploying to gh-pages from @ yichentong16/yichentong16.github.io@409fd005790649b0ec0dac1735e5dd0573930296 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,16 +384,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-graduated-with-a-master-s-degree-high-distinction-from-the-technical-university-of-munich-my-master-s-thesis-is-available-here",
+          title: 'I graduated with a master’s degree (high distinction) from the Technical University of...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-joined-the-algebra-geometry-and-computer-algebra-group-at-rptu-as-a-phd-candidate",
+          title: 'I joined the Algebra, Geometry and Computer Algebra group at RPTU as a...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
